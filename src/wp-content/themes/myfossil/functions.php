e5c94b24252a9b3f1fc45f0a4e177b731a1f0fa0
Joseph Furlott <jmfurlott@gmail.com>
2014-09-12 09:40:02 -0400

    header started

--- src/wp-content/themes/myfossil/functions.php
+++ src/wp-content/themes/myfossil/functions.php
@@ -96,6 +96,8 @@ function myfossil_scripts() {
         wp_enqueue_script('js', get_theme_root_uri().'/assets/staging/myfossil.js');
         wp_enqueue_style('style', get_theme_root_uri().'/assets/staging/style.css');
 
+        wp_enqueue_style('font-awesome', "//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css");
+
 	//wp_enqueue_script( 'myfossil-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
 
 	//wp_enqueue_script( 'myfossil-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
--- src/wp-content/themes/myfossil/header.php
+++ src/wp-content/themes/myfossil/header.php
@@ -20,18 +20,56 @@
 
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed site">
-	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'myfossil' ); ?></a>
+  <a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'myfossil' ); ?></a>
+  
+  <nav class="navbar navbar-inverse" role="navigation">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+          <span class="sr-only">Toggle Navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="/">myFossil</a>
+      </div>
 
-	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-		</div>
+      <!-- Nav links themselves -->
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav">
+          <li class="active"><a href="#">COMMUNITY</a></li>
+          <li><a href="#">FOSSILS</a></li>
+          <li><a href="#">COLLABORATE</a></li>
+          <li><a href="#">RESOURCES</a></li>
+          <li><a href="#">EVENTS</a></li>
+        </ul>
+        
+        <!-- to the right nav -->
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="#"><i class="fa fa-envelope-o"></i></a></li>
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">John Doe <span class="caret"></span></a>
+            <ul class="dropdown-menu" role="menu">
+              <li><a href="#">Action</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div> 
+    </div>
+  </nav>
 
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle"><?php _e( 'Primary Menu', 'myfossil' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
 
-	<div id="content" class="site-content">
+
+  <header id="masthead" class="site-header" role="banner">
+    <div class="site-branding">
+      <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+      <h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+    </div>
+
+    <nav id="site-navigation" class="main-navigation" role="navigation">
+      <button class="menu-toggle"><?php _e( 'Primary Menu', 'myfossil' ); ?></button>
+      <?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+    </nav><!-- #site-navigation -->
+  </header><!-- #masthead -->
+
+  <div id="content" class="site-content">
