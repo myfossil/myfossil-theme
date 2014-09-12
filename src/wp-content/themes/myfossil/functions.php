f9bffde04d39e6ecb4a7897a09c5b6f90b8cdac1
Joseph Furlott <jmfurlott@gmail.com>
2014-09-11 21:12:58 -0400

    removed unnecessary scripts that werent loaded correctly

--- src/wp-content/themes/myfossil/functions.php
+++ src/wp-content/themes/myfossil/functions.php
@@ -96,9 +96,9 @@ function myfossil_scripts() {
         wp_enqueue_script('js', get_theme_root_uri().'/assets/staging/myfossil.js');
         wp_enqueue_style('style', get_theme_root_uri().'/assets/staging/style.css');
 
-	wp_enqueue_script( 'myfossil-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
+	//wp_enqueue_script( 'myfossil-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
 
-	wp_enqueue_script( 'myfossil-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+	//wp_enqueue_script( 'myfossil-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
