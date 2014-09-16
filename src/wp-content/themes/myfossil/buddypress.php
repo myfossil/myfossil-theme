7fcc837eff2e217cac05aed99ff8bae1884db930
Joseph Furlott <jmfurlott@gmail.com>
2014-09-15 23:01:51 -0400

    added buddypress; couple other changes

---
+++ src/wp-content/themes/myfossil/buddypress.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Template Name: Full-width Page Template, For BuddyPress
+ */
+get_header(); ?>
+
+  <div class="user-container">
+    <h5>John Doe</h5>
+
+    <ul>
+      <li class="active"><a href="#">What's New</a></li>
+      <li><a href="#">Wall</a></li>
+      <li><a href="#">Messages</a></li>
+      <li><a href="#">Notifications</a></li>
+    </ul>
+
+    <ul>
+      <li><a href="#">My Fossils</a></li>
+      <li><a href="#">My Activities</a></li>
+      <li><a href="#">My Resources</a></li>
+      <li><a href="#">My Events</a></li>
+      <li><a href="#">My Organizations</a></li>
+    </ul>
+
+    <ul>
+      <li><a href="#">Following</a></li>
+      <li><a href="#">Followers</a></li>
+      <li><a href="#">Edit Profile</a></li>
+      <li><a href="#">Settings</a></li>
+    </ul>
+  </div>
+ 
+
+  <div class="updates-container">
+    <h1>What's New</h1>
+    
+    <h3>ANNOUNCEMENTS</h3>
+    <h3>STATUS UPDATE</h3>
+  </div>
+  
+
+  <div class="externals-container">
+    <h4>FOLLOWING</h4>
+    <h4>ORGANIZATIONS</h4>
+    <h4>RESOURCES</h4>
+  </div>
--- src/wp-content/themes/myfossil/header.php
+++ src/wp-content/themes/myfossil/header.php
@@ -69,18 +69,4 @@
     </div>
   </nav>
 
-
-
-  <header id="masthead" class="site-header" role="banner">
-    <div class="site-branding">
-      <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-      <h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-    </div>
-
-    <nav id="site-navigation" class="main-navigation" role="navigation">
-      <button class="menu-toggle"><?php _e( 'Primary Menu', 'myfossil' ); ?></button>
-      <?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-    </nav><!-- #site-navigation -->
-  </header><!-- #masthead -->
-
   <div id="content" class="site-content">
