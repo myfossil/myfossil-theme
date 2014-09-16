53006036bb1090526b622bfd694e81854ed21cab
Brandon Wood <btwood@geometeor.com>
2014-09-16 11:13:18 -0400

    Updated styles to remove gray background from divs

--- src/wp-content/themes/myfossil/buddypress/activity/index.php
+++ src/wp-content/themes/myfossil/buddypress/activity/index.php
@@ -3,10 +3,12 @@
 <div id="buddypress">
 
 	<?php do_action('bp_before_directory_activity_content'); ?>
-
-	<?php if (is_user_logged_in()): ?>
-		<?php bp_get_template_part('activity/post-form'); ?>
-	<?php endif; ?>
+    
+    <div class="user-container">
+        <?php if (is_user_logged_in()): ?>
+            <?php bp_get_template_part('activity/post-form'); ?>
+        <?php endif; ?>
+    </div>
 
 	<?php do_action('template_notices'); ?>
 
@@ -25,52 +27,55 @@
 				<?php do_action('bp_before_activity_type_tab_friends'); ?>
 
 				<?php if (bp_is_active('friends')): ?>
-
 					<?php if (bp_get_total_friend_count(bp_loggedin_user_id())): ?>
-
-						<li id="activity-friends"><a href="<?php echo bp_loggedin_user_domain() . bp_get_activity_slug() . '/' . bp_get_friends_slug() . '/'; ?>" title="<?php esc_attr_e('The activity of my friends only.', 'buddypress'); ?>"><?php printf(__('My Friends <span>%s</span>', 'buddypress'), bp_get_total_friend_count(bp_loggedin_user_id())); ?></a></li>
-
-					<?php
-        endif; ?>
-
-				<?php
-    endif; ?>
+						<li id="activity-friends">
+                            <a href="<?php echo bp_loggedin_user_domain() . bp_get_activity_slug() . '/' . bp_get_friends_slug() . '/'; ?>" title="<?php esc_attr_e('The activity of my friends only.', 'buddypress'); ?>">
+                                <?php printf(__('My Friends <span>%s</span>', 'buddypress'), bp_get_total_friend_count(bp_loggedin_user_id())); ?>
+                            </a>
+                        </li>
+					<?php endif; ?>
+                <?php endif; ?>
 
 				<?php do_action('bp_before_activity_type_tab_groups'); ?>
 
 				<?php if (bp_is_active('groups')): ?>
-
 					<?php if (bp_get_total_group_count_for_user(bp_loggedin_user_id())): ?>
-
-						<li id="activity-groups"><a href="<?php echo bp_loggedin_user_domain() . bp_get_activity_slug() . '/' . bp_get_groups_slug() . '/'; ?>" title="<?php esc_attr_e('The activity of groups I am a member of.', 'buddypress'); ?>"><?php printf(__('My Groups <span>%s</span>', 'buddypress'), bp_get_total_group_count_for_user(bp_loggedin_user_id())); ?></a></li>
-
-					<?php
-        endif; ?>
-
-				<?php
-    endif; ?>
+						<li id="activity-groups">
+                            <a href="<?php echo bp_loggedin_user_domain() . bp_get_activity_slug() . '/' . bp_get_groups_slug() . '/'; ?>" title="<?php esc_attr_e('The activity of groups I am a member of.', 'buddypress'); ?>">
+                                <?php printf(__('My Groups <span>%s</span>', 'buddypress'), bp_get_total_group_count_for_user(bp_loggedin_user_id())); ?>
+                            </a>
+                        </li>
+                    <?php endif; ?>
+                <?php endif; ?>
 
 				<?php do_action('bp_before_activity_type_tab_favorites'); ?>
 
 				<?php if (bp_get_total_favorite_count_for_user(bp_loggedin_user_id())): ?>
-
-					<li id="activity-favorites"><a href="<?php echo bp_loggedin_user_domain() . bp_get_activity_slug() . '/favorites/'; ?>" title="<?php esc_attr_e("The activity I've marked as a favorite.", 'buddypress'); ?>"><?php printf(__('My Favorites <span>%s</span>', 'buddypress'), bp_get_total_favorite_count_for_user(bp_loggedin_user_id())); ?></a></li>
-
-				<?php
-    endif; ?>
+					<li id="activity-favorites">
+                        <a href="<?php echo bp_loggedin_user_domain() . bp_get_activity_slug() . '/favorites/'; ?>" title="<?php esc_attr_e("The activity I've marked as a favorite.", 'buddypress'); ?>">
+                            <?php printf(__('My Favorites <span>%s</span>', 'buddypress'), bp_get_total_favorite_count_for_user(bp_loggedin_user_id())); ?>
+                        </a>
+                    </li>
+                <?php endif; ?>
 
 				<?php if (bp_activity_do_mentions()): ?>
-
 					<?php do_action('bp_before_activity_type_tab_mentions'); ?>
 
-					<li id="activity-mentions"><a href="<?php echo bp_loggedin_user_domain() . bp_get_activity_slug() . '/mentions/'; ?>" title="<?php esc_attr_e('Activity that I have been mentioned in.', 'buddypress'); ?>"><?php _e('Mentions', 'buddypress'); ?><?php if (bp_get_total_mention_count_for_user(bp_loggedin_user_id())): ?> <strong><span><?php printf(_nx('%s new', '%s new', bp_get_total_mention_count_for_user(bp_loggedin_user_id()), 'Number of new activity mentions', 'buddypress'), bp_get_total_mention_count_for_user(bp_loggedin_user_id())); ?></span></strong><?php
-        endif; ?></a></li>
+					<li id="activity-mentions">
+                        <a href="<?php echo bp_loggedin_user_domain() . bp_get_activity_slug() . '/mentions/'; ?>" title="<?php esc_attr_e('Activity that I have been mentioned in.', 'buddypress'); ?>">
+                            <?php _e('Mentions', 'buddypress'); ?>
+                            <?php if (bp_get_total_mention_count_for_user(bp_loggedin_user_id())): ?> 
+                                <strong>
+                                    <span>
+                                        <?php printf(_nx('%s new', '%s new', bp_get_total_mention_count_for_user(bp_loggedin_user_id()), 'Number of new activity mentions', 'buddypress'), bp_get_total_mention_count_for_user(bp_loggedin_user_id())); ?>
+                                    </span>
+                                </strong>
+                            <?php endif; ?>
+                        </a>
+                    </li>
+				<?php endif; ?>
 
-				<?php
-    endif; ?>
-
-			<?php
-endif; ?>
+			<?php endif; ?>
 
 			<?php do_action('bp_activity_type_tabs'); ?>
 		</ul>
