01737ac97499d8b7649e51fd5e3f360db66b62a7
Brandon Wood <btwood@geometeor.com>
2014-10-03 12:33:21 -0400

    Added back submodules.

--- src/wp-content/themes/myfossil/buddypress/groups/single/admin/membership-requests.php
+++
@@ -1,7 +0,0 @@
-<?php do_action( 'bp_before_group_membership_requests_admin' ); ?>
-
-    <div class="requests">
-        <?php bp_get_template_part( 'groups/single/requests-loop' ); ?>
-    </div>
-
-<?php do_action( 'bp_after_group_membership_requests_admin' ); ?>
