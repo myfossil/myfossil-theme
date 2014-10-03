8f10f84c7bbe93f157e3d0ae1c8764e6fe3895bb
Brandon Wood <btwood@geometeor.com>
2014-10-03 14:33:26 -0400

    Updated script so that it properly updates submodules.

--- git-update.sh
+++ git-update.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 git pull --recurse-submodules && git submodule update --init --recursive
 cp src/wp-config.php.sample src/wp-config.php
+#find . -type d -name vendor -delete
+git submodule foreach git pull origin master
 pushd src/wp-content/plugins/myfossil-specimen && composer install
 popd
 pushd src/wp-content/plugins/myfossil-logging && composer install
--- update-dev.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-git pull --recurse-submodules && git submodule update --init --recursive
-pushd src/wp-content/plugins/myfossil-specimen && composer update
-popd
-pushd src/wp-content/plugins/myfossil-logging && composer update 
-popd
