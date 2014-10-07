f09e468c7b0e7da196a90ca0ce8cef3cd8641106
Brandon Wood <btwood@geometeor.com>
2014-10-07 13:08:33 -0400

    Updated git-update script to use subtrees

--- git-update.sh
+++ git-update.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-git pull --recurse-submodules && git submodule update --init --recursive
-cp src/wp-config.php.sample src/wp-config.php
-#find . -type d -name vendor -delete
-git submodule foreach git pull origin master
-pushd src/wp-content/plugins/myfossil-specimen && composer install
-popd
-pushd src/wp-content/plugins/myfossil-logging && composer install
-popd
+git subtree pull --squash --prefix=src/wp-content/plugins/buddypress buddypress master
+git subtree pull --squash --prefix=src/wp-content/plugins/bbpress bbpress master
+git subtree pull --squash --prefix=src/wp-content/plugins/myfossil-logging plugin-logging master
+git subtree pull --squash --prefix=src/wp-content/plugins/myfossil-specimen plugin-specimen master
+git subtree pull --squash --prefix=src/wp-content/themes/myfossil theme-myfossil master
+git subtree pull --squash --prefix=src/wp-core wordpress master
