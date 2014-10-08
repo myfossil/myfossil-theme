78f5d549f27e54129a1faf97e864928a54aa7b2e
Brandon Wood <btwood@geometeor.com>
2014-10-08 12:54:57 -0400

    Added basic unittests

--- src/wp-content/plugins/myfossil-specimen/tests/test-activate.php
+++ src/wp-content/plugins/myfossil-specimen/tests/test-activate.php
@@ -1,6 +1,8 @@
 <?php
 
-class SampleTest extends WP_UnitTestCase {
+namespace myFOSSIL\Plugin\Specimen;
+
+class ActivateTest extends \WP_UnitTestCase {
 
 	function testSample() {
 		// replace this with some actual testing code
---
+++ src/wp-content/plugins/myfossil-specimen/tests/test-common.php
@@ -0,0 +1,10 @@
+<?php
+namespace myFOSSIL\Plugin\Specimen;
+
+class CommonTest extends \WP_UnitTestCase {
+
+	function testPluginInstantiation() {
+        $p = new myFOSSIL_Specimen;
+	}
+}
+
