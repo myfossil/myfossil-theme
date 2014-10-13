bf37f74c49f87d4c089a33a273cb53f19b61c448
Brandon Wood <btwood@geometeor.com>
2014-10-13 13:04:05 -0400

    Copied local Puppet configs to different environments

---
+++ puppet/dev/modules/mysql/manifests/init.pp
@@ -0,0 +1,85 @@
+class mysql {
+  $mysql_root_passwd = "wp_myfossil"
+  $wp_database = "wp_myfossil"
+  $wp_database_test = "wp_myfossil_test"
+  $wp_db_username = "wp_myfossil"
+  $wp_db_password = "wp_myfossil"
+
+  # Install mysql
+  package { 'mysql-server':
+    ensure => present,
+    require => Exec['apt-get update'],
+  }
+
+  package { 'mysql-client':
+    ensure => present,
+    require => Exec['apt-get update'],
+  }
+
+  # Run mysql
+  service { 'mysql':
+    ensure  => running,
+    require => Package['mysql-server'],
+  }
+
+  # Use a custom mysql configuration file
+  file { '/etc/mysql/my.cnf':
+    source  => 'puppet:///modules/mysql/mysql.conf',
+    require => Package['mysql-server'],
+    notify  => Service['mysql'],
+  }
+
+  # We set the root password here
+  exec { 'set-mysql-password':
+    unless  => "mysql --user=root --password=${mysql_root_passwd} ${wp_databsae} -e exit",
+    command => "mysqladmin --user=root password ${mysql_root_passwd}",
+    path    => ['/bin', '/usr/bin'],
+    require => Service['mysql'],
+  }
+
+  # Create WordPress database
+  exec { 'create-wp-database':
+    unless  => "mysql --user=root --password=${mysql_root_passwd} ${wp_database} -e exit",
+    command => "mysqladmin --user=root --password=${mysql_root_passwd} create ${wp_database}",
+    path    => ['/bin', '/usr/bin'],
+    require => [
+      Service['mysql'], 
+      Package['mysql-client'],
+      Exec['set-mysql-password'],
+    ],
+  }
+
+  # Set WordPress database permissions
+  exec { 'set-wp-database-permissions':
+    command => "mysql --user=root --password=${mysql_root_passwd} ${wp_database} -e \"GRANT ALL PRIVILEGES ON ${wp_database}.* TO \'${wp_db_username}\'@\'%\' IDENTIFIED BY \'${wp_db_password}\'; FLUSH PRIVILEGES;\"",
+    path    => ['/bin', '/usr/bin'],
+    require => [
+      Service['mysql'], 
+      Package['mysql-client'],
+      Exec['create-wp-database']
+    ],
+  }
+
+  # Create WordPress test database
+  exec { 'create-wp-test-database':
+    unless  => "mysql --user=root --password=${mysql_root_passwd} ${wp_database_test} -e exit",
+    command => "mysqladmin --user=root --password=${mysql_root_passwd} create ${wp_database_test}",
+    path    => ['/bin', '/usr/bin'],
+    require => [
+      Service['mysql'], 
+      Package['mysql-client'],
+      Exec['set-mysql-password'],
+    ],
+  }
+
+  # Set WordPress test database permissions
+  exec { 'set-wp-test-database-permissions':
+    command => "mysql --user=root --password=${mysql_root_passwd} ${wp_database_test} -e \"GRANT ALL PRIVILEGES ON ${wp_database_test}.* TO \'${wp_db_username}\'@\'%\' IDENTIFIED BY \'${wp_db_password}\'; FLUSH PRIVILEGES;\"",
+    path    => ['/bin', '/usr/bin'],
+    require => [
+      Service['mysql'], 
+      Package['mysql-client'],
+      Exec['create-wp-database']
+    ],
+  }
+}
---
+++ puppet/dev/modules/nginx/manifests/init.pp
@@ -0,0 +1,47 @@
+class nginx {
+  # Symlink /var/www/app on our guest with 
+  # host /path/to/vagrant/app on our system
+  file { '/var/www/myfossil':
+    ensure  => 'link',
+    target  => '/vagrant/src',
+  }
+
+  # Install the nginx package. This relies on apt-get update
+  package { 'nginx':
+    ensure => 'present',
+    require => Exec['apt-get update'],
+  }
+
+  # Make sure that the nginx service is running
+  service { 'nginx':
+    ensure => running,
+    require => Package['nginx'],
+  }
+
+  # Add a vhost template
+  file { 'vagrant-nginx':
+    path => '/etc/nginx/sites-available/myfossil',
+    ensure => file,
+    require => Package['nginx'],
+    source => 'puppet:///modules/nginx/myfossil.conf',
+  }
+
+  # Disable the default nginx vhost
+  file { 'default-nginx-disable':
+    path => '/etc/nginx/sites-enabled/default',
+    ensure => absent,
+    require => Package['nginx'],
+  }
+
+  # Symlink our vhost in sites-enabled to enable it
+  file { 'vagrant-nginx-enable':
+    path => '/etc/nginx/sites-enabled/myfossil',
+    target => '/etc/nginx/sites-available/myfossil',
+    ensure => link,
+    notify => Service['nginx'],
+    require => [
+      File['vagrant-nginx'],
+      File['default-nginx-disable'],
+    ],
+  }
+}
