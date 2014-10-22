29c4a8aafb43f84d685dab34b56572686d75e435
Brandon Wood <btwood@geometeor.com>
2014-10-22 13:34:55 -0400

    Added placeholder places page

---
+++ page-places.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Template Name: Places
+ *
+ * The template for displaying all Places.
+ *
+ * @package myfossil
+ */
+get_header(); 
+
+use \myFOSSIL\Plugin\Resources;
+
+
+?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+                
+        </main>
+    </div>
+
+<?php get_footer(); ?>
