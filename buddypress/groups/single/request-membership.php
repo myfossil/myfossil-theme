1c06fc6a8dbf0855a9e154c86e90c5636bf0cbff
Brandon Wood <btwood@geometeor.com>
2014-11-05 09:59:17 -0500

    Wrapped Request Membership form in Bootstrap. Closes #82.

--- buddypress/groups/single/request-membership.php
+++ buddypress/groups/single/request-membership.php
@@ -1,15 +1,18 @@
 <?php do_action( 'bp_before_group_request_membership_content' ); ?>
 
 <?php if ( !bp_group_has_requested_membership() ) : ?>
-	<p><?php printf( __( "You are requesting to become a member of the group '%s'.", "buddypress" ), bp_get_group_name( false ) ); ?></p>
+	<p><?php printf( __( "You are requesting to become a member of the group <strong>%s</strong>.", "buddypress" ), bp_get_group_name( false ) ); ?></p>
+    <p>You have the option to include a short message to the organizer.</p>
 
-	<form action="<?php bp_group_form_action('request-membership' ); ?>" method="post" name="request-membership-form" id="request-membership-form" class="standard-form">
-		<label for="group-request-membership-comments"><?php _e( 'Comments (optional)', 'buddypress' ); ?></label>
-		<textarea name="group-request-membership-comments" id="group-request-membership-comments"></textarea>
+	<form action="<?php bp_group_form_action('request-membership' ); ?>" method="post" name="request-membership-form" id="request-membership-form" class="form standard-form">
+        <div class="form-group">
+            <label class="control-label" for="group-request-membership-comments"><?php _e( 'Comments (optional)', 'buddypress' ); ?></label>
+            <textarea class="form-control" name="group-request-membership-comments" id="group-request-membership-comments"></textarea>
+        </div>
 
 		<?php do_action( 'bp_group_request_membership_content' ); ?>
 
-		<p><input type="submit" name="group-request-send" id="group-request-send" value="<?php esc_attr_e( 'Send Request', 'buddypress' ); ?>" />
+		<input type="submit" class="btn btn-default" name="group-request-send" id="group-request-send" value="<?php esc_attr_e( 'Send Request', 'buddypress' ); ?>" />
 
 		<?php wp_nonce_field( 'groups_request_membership' ); ?>
 	</form><!-- #request-membership-form -->
