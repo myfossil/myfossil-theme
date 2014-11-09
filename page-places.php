7057647a7565e3cd0e5759824382ab7e813d24b0
Joseph Furlott <jmfurlott@gmail.com>
2014-11-09 12:54:25 -0500

    change #state to #state-filter

--- page-places.php
+++ page-places.php
@@ -184,7 +184,7 @@ get_header();
                 <form role="form" id="filters">
                     <?php wp_nonce_field( 'myfr_filter', 'myfr_filter_nonce' ); ?>
                     <h4>State</h4>
-                    <select class="form-control" id="state">
+                    <select class="form-control" id="state-filter">
                     </select>
 
                     <h4>Type</h4>
