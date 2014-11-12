a9ea6a7b97291f3a07d4d43081e6ce8accc1d295
Brandon Wood <btwood@geometeor.com>
2014-11-12 13:28:23 -0500

    Added new CSS classes for Place and Event boxes.
    Closes #76.
    Closes #77.

--- page-events.php
+++ page-events.php
@@ -18,11 +18,11 @@ get_header();
 <!-- {{{ events template -->
 <script id="tpl-events" type="text/x-handlebars-template">
 {{#each events}}
-    <div class="panel panel-default col-xs-12 col-sm-12 col-md-6" 
+    <div class="event col-xs-12 col-sm-12 col-md-6" 
             data-event-state="{{ state }}"
             data-event-type="{{ type }}"
             data-event-date="{{ month_year }}">
-        <div class="panel-body">
+        <div class="event-body">
             <h5 class="pull-left">{{ title }}</h5>
             <p class="pull-right">
                 <i class="fa fa-fw fa-clock-o"></i> 
