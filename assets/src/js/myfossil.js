fd2490c4e04c4fc7bb80a9b495694a90e1c8b772
Brandon Wood <btwood@geometeor.com>
2014-11-22 13:57:50 -0500

    Beautified JavaScript files

--- assets/src/js/myfossil.js
+++ assets/src/js/myfossil.js
@@ -1,1 +1,13 @@
-function throttle(d,a,h){a||(a=250);var b,e;return function(){var f=h||this,c=+new Date,g=arguments;b&&c<b+a?(clearTimeout(e),e=setTimeout(function(){b=c;d.apply(f,g)},a)):(b=c,d.apply(f,g))}};
+function throttle(d, a, h) {
+    a || (a = 250);
+    var b, e;
+    return function() {
+        var f = h || this,
+            c = +new Date,
+            g = arguments;
+        b && c < b + a ? (clearTimeout(e), e = setTimeout(function() {
+            b = c;
+            d.apply(f, g)
+        }, a)) : (b = c, d.apply(f, g))
+    }
+};
