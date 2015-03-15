64b30f3a3fa05fe7a48b87c1a2bac32290115d2d
Brandon Wood <btwood@geometeor.com>
2015-03-15 13:55:32 -0400

    BUG: Fixed issue with Activity filters not working properly
    Closes #152

--- buddypress/activity/activity-loop.php
+++ buddypress/activity/activity-loop.php
@@ -1,6 +1,6 @@
 <?php do_action('bp_before_activity_loop'); ?>
 
-<?php if ( bp_has_activities() ) : ?>
+<?php if ( bp_has_activities( bp_ajax_querystring( 'activity') ) ) : ?>
     <?php if (empty($_POST['page'])): ?>
         <ul id="activity-stream" class="activity-list item-list">
     <?php endif; ?>
