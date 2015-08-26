ce2fc7487f4554a11c1d42038e3ef5629eb1ef25
Eric Poirier <epoirier@atmoapps.com>
2015-08-26 15:57:16 -0400

    Added message to forums view per FOSSIL team request

--- bbpress/content-archive-forum.php
+++ bbpress/content-archive-forum.php
@@ -23,6 +23,13 @@
 
 	<?php bbp_breadcrumb(); ?>
 
+		<div class="row">
+			<div class="col-xs-10 col-xs-offset-1" id="forum-request-instructions">
+				<p>To reduce confusion, we have a formal process for adding new forums. To request a new Forum, please send an email with a detailed explanation to the myFOSSIL webmaster at <a href="mailto:fossil@flmnh.ufl.edu">fossil@flmnh.ufl.edu</a>.</p>
+			</div>
+		</div>
+
+
 	<?php bbp_forum_subscription_link(); ?>
 
 	<?php do_action( 'bbp_template_before_forums_index' ); ?>
