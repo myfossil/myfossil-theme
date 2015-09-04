c084904d060a5722cf7728530e59dfd4984249f7
mikolaj <mikolaj@atmoapps.com>
2015-09-04 16:04:25 +0200

    #226  Change "Join Group" label in Groups

--- buddypress/groups/filters.php
+++ buddypress/groups/filters.php
@@ -6,7 +6,7 @@
  */
 function filter_bp_group_join_button( $btn ) {
     $btn_txt_leave = 'Leave Group';
-    $btn_txt_join = 'Join Group';
+    $btn_txt_join = 'Follow Group';
     $is_member = $btn['link_text'] == 'Leave Group';
 
     $btn['must_be_logged_in'] = true;
