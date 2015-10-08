3c0e77bf234ee3c1ec038e37da10dae0037df761
Eric Poirier <epoirier@atmoapps.com>
2015-10-08 13:49:36 -0400

    Removed category links
    closes #253

--- single-document.php
+++ single-document.php
@@ -17,24 +17,13 @@ get_header();
 		<div class="">
 			<br/>
 
-			<div class="">
-			<nav class="nav-single">
-				<span class="nav-previous"><?php previous_post_link( '%link', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', 'bp-simple_events' ) . '</span> %title' ); ?></span>
-				&nbsp; &nbsp;
-				<span class="nav-next"><?php next_post_link( '%link', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'bp-simple_events' ) . '</span>' ); ?></span>
-			</nav><!-- .nav-single -->
-		</div>
-
 			<h2 class="entry-title">
 				<a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>">
 				<?php the_title(); ?></a>
 			</h2>
 			<p class="category">
-<strong>	Category: </strong> <?php the_category(', ') ?>
-</p>
-
-
-			
+				<strong>	Category: </strong> <?php the_category(', ') ?>
+			</p>
 
 			<?php
 			if ( has_post_thumbnail() ) {
